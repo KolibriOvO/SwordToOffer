d4ceb28d61be95cad4ee27337a0cd4dea8096a60
KolibirOvO <vigilanteovo@gmail.com>
2023-07-01 15:47:49 +0800

    JZ11

---
+++ JZ11 旋转数组的最小数字.js
@@ -0,0 +1,8 @@
+function minNumberInRotateArray(rotateArray)
+{
+    // write code here
+    return Math.min(...rotateArray);
+}
+module.exports = {
+    minNumberInRotateArray : minNumberInRotateArray
+};
