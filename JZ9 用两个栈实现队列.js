3641e6d9e7ce2dd274ce24fa066a3ce0e7c2a519
KolibirOvO <vigilanteovo@gmail.com>
2023-07-01 15:47:41 +0800

    JZ9

---
+++ JZ9 用两个栈实现队列.js
@@ -0,0 +1,22 @@
+let stack1 = [];
+let stack2 = [];
+
+function push(node) {
+    // write code here
+    stack1.push(node);
+}
+
+function pop() {
+    // write code here
+    if (!stack2.length) {
+        while (stack1.length) {
+            stack2.push(stack1.pop())
+        }
+    }
+    return stack2.pop();
+}
+
+module.exports = {
+    push: push,
+    pop: pop
+};
