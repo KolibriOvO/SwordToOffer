752a32be8b6aed35d166596d205126493bc4812e
KolibirOvO <vigilanteovo@gmail.com>
2023-07-03 22:18:00 +0800

    JZ17

---
+++ JZ17 打印从1到最大的n位数.js
@@ -0,0 +1,20 @@
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param n int整型 最大位数
+ * @return int整型一维数组
+ */
+function printNumbers(n) {
+    // write code here
+    let arr = [];
+    let x = Math.pow(10, n)
+    for (let i = 1; i < x; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+
+module.exports = {
+    printNumbers: printNumbers
+};
