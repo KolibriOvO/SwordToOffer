0b3718727588f8f8796a20879b2855c327f0b1d3
kolibri <vigilanteovo@gmail.com>
2023-07-10 22:51:51 +0800

    JZ84

---
+++ JZ84 二叉树中和为某一值的路径(三).js
@@ -0,0 +1,43 @@
+/*
+ * function TreeNode(x) {
+ *   this.val = x;
+ *   this.left = null;
+ *   this.right = null;
+ * }
+ */
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param root TreeNode类
+ * @param sum int整型
+ * @return int整型
+ */
+let res = 0
+
+function FindPath(root, sum) {
+    // write code here
+    if (root === null) {
+        return 0
+    }
+    dfs(root, sum)
+    FindPath(root.left, sum)
+    FindPath(root.right, sum)
+    return res
+}
+
+function dfs(root, sum) {
+    if (root === null) {
+        return
+    }
+    sum -= root.val
+    if (sum === 0) {
+        res++
+    }
+    dfs(root.left, sum)
+    dfs(root.right, sum)
+}
+
+module.exports = {
+    FindPath: FindPath
+};
